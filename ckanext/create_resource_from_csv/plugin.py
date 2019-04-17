36a10a3d6f4dcc9d54bff29f7e7e661760c7512f
x-malet <xavier.malet.1@ulaval.ca>
2019-04-17 10:14:50 -0400

    modify blueprints in plugin.py

--- ckanext/create_resource_from_csv/plugin.py
+++ ckanext/create_resource_from_csv/plugin.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-__description__= """ Ce plugin permet à l'application CKAN de moissonner un répertoire contenant des fichiers CSV.
+__description__ = """ Ce plugin permet à l'application CKAN de moissonner un répertoire contenant des fichiers CSV.
 
 Le chemin d'accès au répertoire doit être identifié à l'aide d'une variable d'environnement : METADATA_FOLDER
 """
@@ -14,15 +14,17 @@ CURRENT_PATH = environ['METADATA_FOLDER']
 
 app = Blueprint('file_server', __name__)
 
+
 def get_folder_content(guid):
     if guid in listdir(CURRENT_PATH):
-        folder_content =listdir(path.join(CURRENT_PATH, guid))
+        folder_content = listdir(path.join(CURRENT_PATH, guid))
         xml_file = "{}.xml".format(guid)
         if xml_file in folder_content:
             folder_content.remove(xml_file)
             return folder_content
     else:
         return False
+
 
 def get_folder_by_index(index):
     print("in get by index")
@@ -33,59 +35,56 @@ def get_folder_by_index(index):
         return data_content[index]
 
 
-
 @app.route("/data")
-@app.route("/data/")
 def get_data_content():
-    return jsonify({'data_content':listdir(CURRENT_PATH)})
+    return jsonify({'data_content': listdir(CURRENT_PATH)})
 
 
 @app.route("/data/<string:guid>")
-@app.route("/data/<string:guid>/")
 def get_content(guid):
     content = get_folder_content(guid)
     if content:
-        return jsonify({'folder_content':content})
+        return jsonify({'folder_content': content})
     else:
         return 'Folder not found. Check metadata.'
 
+
 @app.route("/data/<int:index>")
-@app.route("/data/<int:index>/")
 def get_index(index):
     data_content = listdir(CURRENT_PATH)
     if index > len(data_content):
         return abort(404)
     else:
         guid = data_content[index]
-        return redirect(url_for('file_server.get_content',guid=guid))
+        return redirect(url_for('file_server.get_content', guid=guid))
 
 
 @app.route("/data/<string:guid>/<string:file_name>")
-@app.route("/data/<string:guid>/<string:file_name>/")
 def get_file(guid, file_name):
     folder_content = get_folder_content(guid)
     if folder_content:
         if file_name in folder_content:
-            return send_from_directory(path.join(CURRENT_PATH, guid),file_name)
+            return send_from_directory(path.join(CURRENT_PATH, guid), file_name)
     else:
         return 'Folder not found. Check metadata.'
 
+
 @app.route("/data/<int:folder_index>/<string:file_name>")
-@app.route("/data/<int:folder_index>/<string:file_name>/")
 def get_file_by_folder_index(folder_index, file_name):
     print('get_file_by_folder_index')
     guid = get_folder_by_index(folder_index)
     if guid:
-        return redirect(url_for('file_server.get_file',guid=guid,file_name=file_name))
+        return redirect(url_for('file_server.get_file', guid=guid, file_name=file_name))
+
 
 @app.route("/data/<string:guid>/<int:file_index>")
-@app.route("/data/<string:guid>/<int:file_index>/")
 def get_file_by_index_in_folder(guid, file_index):
     print('get_file_by_index_in_folder')
     folder_content = get_folder_content(guid)
     if folder_content and file_index < len(folder_content):
         file_name = folder_content[file_index]
-        return redirect(url_for('file_server.get_file',guid=guid,file_name=file_name))
+        return redirect(url_for('file_server.get_file', guid=guid, file_name=file_name))
+
 
 class Create_Resource_From_CsvPlugin(p.SingletonPlugin):
     p.implements(p.IBlueprint)
